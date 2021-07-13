ac9a684598201843ceac336411c2b40dc73a590b
Roy Moore <roy@moore.co.il>
2021-07-13 14:44:31 +0300

    improve basic python script

--- src/service.py
+++ src/service.py
@@ -1,5 +1,10 @@
 def main():
     print("this is a test")
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:  # interrupt it with Ctrl-C (SIGINT)
+        pass
 
 
 if __name__ == "__main__":
